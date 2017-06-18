4b2c59c659f721cd876368544b2e2732ea6f7c0b
job4pak <job4pakteam@gmail.com>
2017-06-18 22:32:43 +0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'home/help'
+
+  get 'home/contact'
+
+  get 'home/aboutus'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
